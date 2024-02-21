a52a2f395dd87abbfc75b1410960f3f0e27c2a82
AnasNdeem <126314120+AnasNdeem@users.noreply.github.com>
2024-02-21 08:14:27 -0800

    Add files via upload

---
+++ 01-basic.js
@@ -0,0 +1,12 @@
+const accoutId = 12333; 
+let accountEmail = "anas9deem@gmail.com";
+var accountPassword = "0999$";
+let accountState;
+accountCity = "Lahore";
+//accoutId = 111;
+accountEmail = "anasmaher8900@gmail.com";
+accountPassword = "9800$";
+accountCity = "Karachi";
+
+document.write(accoutId);
+console.table([accoutId, accountEmail, accountPassword, accountState, accountCity]);
---
+++ 02-basic.js
@@ -0,0 +1,8 @@
+// number => to power 53
+// bigint
+// string => ""
+// boolean => standalone  value 
+// undefiend = > 
+ let age = 20;
+ let isLoggedIn = false;
+ let state; 
